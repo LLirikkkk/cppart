998025c7b58a485306f2f3c1ad3411b3aad816f9
Kirill Kotlyar <kirill.kotlyar17@gmail.com>
2026-04-29 00:03:23 +0300

    dev: ThreadPool completed

--- include/sched/thread-pool.h
+++ include/sched/thread-pool.h
@@ -1,32 +1,64 @@
 #pragma once
 
+#include "boost/intrusive/list.hpp"
 #include "resumable.h"
 #include "scheduler.h"
 
+#include <condition_variable>
 #include <cstddef>
+#include <thread>
+#include <vector>
 
 namespace art::sched {
 
-    class ThreadPool final : public IntrusiveListScheduler {
-    public:
-        explicit ThreadPool(std::size_t threads);
+/**
+ * @brief Fixed sized thread pool scheduler.
+ * @note All tasks that were spawned but not executed will be dropped without being resumed on destruction.
+ */
+class ThreadPool final : public IntrusiveListScheduler {
+public:
+  /**
+   * @brief Constructs thread pool with provided number of workers.
+   * @param threads Number of workers.
+   * @throws std::invalid_argument if threads = 0.
+   */
+  explicit ThreadPool(std::size_t threads);
 
-        ~ThreadPool();
+  ~ThreadPool() override;
 
-        // Non-copyable
-        ThreadPool(const ThreadPool&) = delete;
-        ThreadPool& operator=(const ThreadPool&) = delete;
+  ThreadPool(const ThreadPool&) = delete;
 
-        // Non-moveable
-        ThreadPool(ThreadPool&&) = delete;
-        ThreadPool& operator=(ThreadPool&&) = delete;
+  ThreadPool& operator=(const ThreadPool&) = delete;
 
-        void run();
+  ThreadPool(ThreadPool&&) = delete;
 
-        void spawn(Resumable<IntrusiveListScheduler>& task) final;
+  ThreadPool& operator=(ThreadPool&&) = delete;
 
-    private:
-        /* Not implemented */
-    };
+  /**
+   * @brief Starts workers.
+   * @note Must be called at most once.
+   * @note Thread-safety: not thread-safe and must not be called concurrently.
+   */
+  void run();
+
+  /**
+   * @brief Enqueues task for later execution.
+   * @param task Task to enqueue.
+   * @note Task must not be currently queued in any scheduler.
+   * @note Thread-safety: thread-safe.
+   */
+  void spawn(Resumable<IntrusiveListScheduler>& task) noexcept override;
+
+private:
+  void worker_loop(const std::stop_token& st) noexcept;
+
+  std::size_t num_of_workers_ = 0;
+  std::vector<std::jthread> workers_;
+
+  std::mutex mutex_;
+  boost::intrusive::list<Resumable<IntrusiveListScheduler>> queue_;
+
+  std::condition_variable cv_;
+};
 
 } // namespace art::sched
--- src/sched/thread-pool.cpp
+++ src/sched/thread-pool.cpp
@@ -1,9 +1,75 @@
 #include "sched/thread-pool.h"
 
 namespace art::sched {
-ThreadPool::ThreadPool(std::size_t threads) {
+
+ThreadPool::ThreadPool(const std::size_t threads) {
   if (threads == 0) {
     throw std::invalid_argument("Number of threads must be positive");
   }
+
+  num_of_workers_ = threads;
 }
+
+ThreadPool::~ThreadPool() {
+  for (auto& worker : workers_) {
+    worker.request_stop();
+  }
+
+  {
+    std::unique_lock lock(mutex_);
+
+    cv_.notify_all();
+  }
+
+  for (auto& worker : workers_) {
+    worker.join();
+  }
+
+  queue_.clear();
+}
+
+void ThreadPool::run() {
+  assert(workers_.empty());
+
+  workers_.reserve(num_of_workers_);
+  for (std::size_t i = 0; i < num_of_workers_; ++i) {
+    workers_.emplace_back([this](const std::stop_token& st) {
+      worker_loop(st);
+    });
+  }
+}
+
+void ThreadPool::spawn(Resumable<IntrusiveListScheduler>& task) noexcept {
+  {
+    std::unique_lock lock(mutex_);
+
+    queue_.push_back(task);
+  }
+
+  cv_.notify_one();
+}
+
+void ThreadPool::worker_loop(const std::stop_token& st) noexcept {
+  while (true) {
+    Resumable<IntrusiveListScheduler>* task = nullptr;
+
+    {
+      std::unique_lock lock(mutex_);
+
+      cv_.wait(lock, [this, &st] {
+        return !queue_.empty() || st.stop_requested();
+      });
+
+      if (st.stop_requested()) {
+        return;
+      }
+
+      task = &queue_.front();
+      queue_.pop_front();
+    }
+
+    task->resume(*this);
+  }
+}
+
 } // namespace art::sched
