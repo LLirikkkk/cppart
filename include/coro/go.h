b85510330a5bd18fd9633ccd2e4dccee9fff7223
Kirill Kotlyar <kirill.kotlyar17@gmail.com>
2026-04-30 18:04:36 +0300

    dev: go completed

--- include/coro/go.h
+++ include/coro/go.h
@@ -1,11 +1,18 @@
 #pragma once
 
+#include "coroutine.h"
+
 namespace art::coro {
 
-    template <typename Scheduler, typename Routine>
-    void go(Scheduler& scheduler, const Routine& routine);
+template <typename Scheduler, typename Routine>
+void go(Scheduler& scheduler, const Routine& routine) {
+  scheduler.spawn(routine().promise());
+}
 
-    template <typename Routine>
-    void go(const Routine& routine);
+template <typename Routine>
+void go(const Routine& routine) {
+  const auto& curr_coro = Coroutine::current();
+  curr_coro.get_execution_context().SpawnToScheduler(routine());
+}
 
 } // namespace art::coro
